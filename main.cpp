29eb672e899b8c06e3ff405c70421681e052a9a9
PieMar03 <115477227+PieMar03@users.noreply.github.com>
2022-11-27 19:13:14 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-   return 0;
+int main(){
+    int n;
+    int p=1,s=1;
+    cin>>n;
+    cout<<primo<<endl<<secondo<<endl;
+    int i=3;
+    while((i++<=n)&&((primo+secondo)<=n))
+        {
+            cout<<primo+secondo<<endl;
+            if(primo<secondo)
+            {
+                primo=secondo+primo;
+            }
+            else
+            {
+               secondo=primo+secondo;
+            }
+        }
+    return 0;
 }
